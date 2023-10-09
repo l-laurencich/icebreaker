5e9b6119c6ce88ce049815267560af9f6728f140
lucas laurencich <lucas.maximiliano89@gmail.com>
2023-10-09 14:14:31 +0200

    first commit

---
+++ quiz.rb
@@ -0,0 +1,21 @@
+rows = [1,2,3,4]
+columns = ["A", "B", "C", "D", "E"]
+ALL_FIELDS = []
+
+rows.product(columns).each do |e| 
+  ALL_FIELDS << "#{e[1]} #{e[0]}"
+end
+
+
+def pick_one
+  gets
+  puts "drawing a field..."
+  sleep 1
+  current = ALL_FIELDS.sample
+  puts current
+  ALL_FIELDS.delete(current)
+end
+
+loop do
+  pick_one
+end
